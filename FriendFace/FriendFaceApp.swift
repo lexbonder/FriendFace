03e913b0d3f8bc0f76d839c8ac909dcef9a5f1e6
Alex Bonder <lexbonder@gmail.com>
2023-08-16 14:16:46 -0400

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Alex Bonder on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
